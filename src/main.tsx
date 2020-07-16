3813c716d7277a57d6dfacd374ef4f57f90ddc83
Liu Song <song940@gmail.com>
2020-07-16 23:47:30 +0800

    Initial commit

---
+++ src/main.tsx
@@ -0,0 +1,59 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import {
+  HashRouter as Router, Route, Link
+} from "react-router-dom";
+import Loadable from "react-loadable";
+import Header from './components/Header';
+
+import './main.css';
+
+const loading = () => <div />
+
+const routes = [
+  {
+    path: "/",
+    exact: true,
+    component: Loadable({
+      loading,
+      loader: () => import('./pages/index')
+    })
+  },
+  {
+    path: "/home",
+    component: Loadable({
+      loading,
+      loader: () => import('./pages/home')
+    })
+  }
+];
+
+const RouteWithSubRoutes = route => (
+  <Route
+    exact={route.exact}
+    path={route.path}
+    render={props => (
+      <route.component {...props} routes={route.routes} />
+    )}
+  />
+);
+
+const links = [
+  { text: 'Index', href: "/" },
+  { text: 'Home', href: "/home" },
+];
+
+const App = () => {
+  const { } = this.state;
+  return (
+    <Router>
+      <Header title="React App with Router" links={ links } />
+      <div className="content">
+        {routes.map((route, i) => <RouteWithSubRoutes key={i} {...route} />)}
+      </div>
+    </Router>
+  );
+};
+
+ReactDOM.render(<App />,
+  document.getElementById('app'));
